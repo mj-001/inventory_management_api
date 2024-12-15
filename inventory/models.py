3147c60ceb48943231d00a15aecd5fccf5caddb1
john <jmnjogo@gmail.com>
2024-12-15 23:45:33 +0300

    core functionalities

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class InventoryItem(models.Model):
+    name=models.CharField(max_length=250)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=6,decimal_places=2)
+    quantity=models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import InventoryItem
+from .serializers import InventoryItemSerializer
 
-# Create your views here.
+class InventoryListView(APIView):
+    def get(self, request):
+        items = InventoryItem.objects.all()
+        serializer = InventoryItemSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = InventoryItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
