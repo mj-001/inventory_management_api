f64afc004d36f1f78f96470fe8b012ba11e047cc
john <jmnjogo@gmail.com>
2025-01-05 16:51:36 +0300

    pythonanywhere

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -5,7 +5,7 @@ from .models import InventoryItem
 class InventoryItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = InventoryItem
-        fields = ['id', 'name', 'description', 'price', 'quantity', 'managed_by']
+        fields = ['id', 'name', 'description', 'price', 'quantity', 'category', 'managed_by']
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
